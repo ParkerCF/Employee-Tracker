58a09a6eb0272da71891c1598abef1169437df2a
ParkerCF <110678158+ParkerCF@users.noreply.github.com>
2023-01-22 14:55:48 -0500

    adding data to seed.sql

---
+++ db/seed.sql
@@ -0,0 +1,14 @@
+use employees;
+
+INSERT INTO department
+    (name)
+VALUES
+    ('Sales'),
+    ('Engineering'),
+    ('Finance'),
+    ('Legal');
+
+INSERT INTO role
+    (title, salary, department_id)
+VALUES
+    
