1386307f508809cf2f70b163eac72127a1c24e0b
ParkerCF <110678158+ParkerCF@users.noreply.github.com>
2023-01-22 15:00:39 -0500

    updating seed.sql

--- db/seed.sql
+++ db/seed.sql
@@ -11,4 +11,16 @@ VALUES
 INSERT INTO role
     (title, salary, department_id)
 VALUES
+    ('Director of Sales', 120000, 1),
+    ('Account Executive', 75000, 1),
+    ('Engineering Manager', 200000, 2),
+    ('Software Engineer', 150000, 2),
+    ('Financial Account Manager', 150000, 3),
+    ('Accountant', 120000, 3),
+    ('General Counsel', 300000, 4),
+    ('Compliance Officer', 150000, 4);
+
+INSERT INTO employee
+    (first_name, last_name, role_id, manager_id)
+VALUES 
     
